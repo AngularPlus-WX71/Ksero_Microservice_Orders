71642cac1a5ff079d6e6d7a1a873bef9d04c44b6
Caleb <reateguicevallosc@gmail.com>
2022-09-29 19:37:07 -0500

    Initial commit

---
+++ src/main/java/com/example/backend_ksero_orders/BackendKseroOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_ksero_orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKseroOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendKseroOrdersApplication.class, args);
+    }
+
+}

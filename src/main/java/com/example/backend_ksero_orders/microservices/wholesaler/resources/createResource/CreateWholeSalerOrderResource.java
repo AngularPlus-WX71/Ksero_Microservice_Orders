8fe5de10048365670dad3efc5a3cf545ab7630fc
Caleb <reateguicevallosc@gmail.com>
2022-09-30 08:50:02 -0500

    Add: Missing Getter & Setter Annotations

--- src/main/java/com/example/backend_ksero_orders/microservices/wholesaler/resources/createResource/CreateWholeSalerOrderResource.java
+++ src/main/java/com/example/backend_ksero_orders/microservices/wholesaler/resources/createResource/CreateWholeSalerOrderResource.java
@@ -1,8 +1,12 @@
 package com.example.backend_ksero_orders.microservices.wholesaler.resources.createResource;
 
-import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotNull;
 
+@Getter
+@Setter
 public class CreateWholeSalerOrderResource {
 
     @NotNull
